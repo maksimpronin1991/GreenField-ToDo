4eeeebcedee57934e903a4b81dcd549d5048f872
MaksimPro <maksimpronin1991@icloud.com>
2025-02-08 19:27:32 +0300

    feat: start page

---
+++ src/pages/start-page.jsx
@@ -0,0 +1,9 @@
+export function StartPage() {
+    return (
+        <>
+            <h1>Eisenhower Matrix To-Do List</h1>
+            <button className="start-btn">Lets start</button>
+        </>
+    )
+
+}
