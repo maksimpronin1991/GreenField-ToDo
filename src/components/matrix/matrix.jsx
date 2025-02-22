05c19db5e7b787aca4e063085a54e5a9ba96f789
MaksimPro <maksimpronin1991@icloud.com>
2025-02-22 17:17:12 +0300

    style: add litle instruction for matrix title

--- src/components/matrix/matrix.jsx
+++ src/components/matrix/matrix.jsx
@@ -3,20 +3,20 @@ import { Task } from "../task/task";
 export function Matrix(){
     return (<div className="matrix">
         <div className="quadrant urgent-important">
-          <h2>Urgent &amp; Important</h2>
+          <h2>Urgent &amp; Important (make it now)</h2>
           <ul id="urgent-important-list">
             <Task/>
           </ul>
         </div>
         <div className="quadrant not-urgent-important">
-          <h2>Not Urgent &amp; Important</h2>
+          <h2>Not Urgent &amp; Important (plane it)</h2>
           <ul id="not-urgent-important-list">
           <Task/>
 
           </ul>
         </div>
         <div className="quadrant urgent-not-important">
-          <h2>Urgent &amp; Not Important</h2>
+          <h2>Urgent &amp; Not Important (delegate it)</h2>
           <ul id="urgent-not-important-list">
           <Task/>
           <Task/>
@@ -28,7 +28,7 @@ export function Matrix(){
           </ul>
         </div>
         <div className="quadrant not-urgent-not-important">
-          <h2>Not Urgent &amp; Not Important</h2>
+          <h2>Not Urgent &amp; Not Important (delete or put it aside)</h2>
           <ul id="not-urgent-not-important-list">
           <Task/>
 
